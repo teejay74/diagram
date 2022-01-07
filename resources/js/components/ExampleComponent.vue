899ff3a3e140817249b50935e8e6df7317fa5b69
teejay74 <banka84@ya.ru>
2022-01-07 10:46:18 +0500

    add table and vue3ChartJs

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                       123121223
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
